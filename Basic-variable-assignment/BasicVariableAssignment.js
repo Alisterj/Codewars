a9440a6d02e4f050b3f4db136f72a052c2c389ec
Alisterj <lisarsiss@gmail.com>
2021-12-07 05:10:01 +0300

    Kata Basic variable assignment (8kuy)

---
+++ Basic-variable-assignment/BasicVariableAssignment.js
@@ -0,0 +1,6 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+var a = "code"; // a == "code" 
+var b = "wa.rs"; // b == "wa.rs"
+var name = a + b; // name == a + b
