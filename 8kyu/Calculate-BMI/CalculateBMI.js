8c013992e1ab1809fa343a7662c718af908d65ae
Alisterj <lisarsiss@gmail.com>
2021-12-08 11:40:31 +0300

    Kata Calculate BMI (8kyu)

---
+++ 8kyu/Calculate-BMI/CalculateBMI.js
@@ -0,0 +1,18 @@
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+
+function bmi(weight, height) {
+	return (weight / Math.pow(height, 2) <= 18.5 ?
+		"Underweight" : weight / Math.pow(height, 2) <= 25.0 ?
+			"Normal" : weight / Math.pow(height, 2) <= 30.0 ?
+				"Overweight" : "Obese");
+}
