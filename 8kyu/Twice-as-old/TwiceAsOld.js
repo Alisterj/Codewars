c6dbac0254aaf79ae02f2ed157f6ee5621a14d72
Alisterj <lisarsiss@gmail.com>
2021-12-08 12:19:25 +0300

    Kata Twice as old (8kyu)

--- 8kyu/Twice-as-old/TwiceAsOld.js
+++ 8kyu/Twice-as-old/TwiceAsOld.js
@@ -5,5 +5,4 @@ current age of his son (years)
 Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
 */
 
-
 const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2);
