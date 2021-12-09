11627be5e84898b806bc1d288a346acaf2aa9d9e
Alisterj <lisarsiss@gmail.com>
2021-12-09 10:52:22 +0300

    Kata Grasshopper - Variable Assignment Debug (8kyu)

---
+++ 8kyu/Grasshopper-Variable-Assignment-Debug/GrasshopperVariableAssignmentDebug.js
@@ -0,0 +1,15 @@
+/*Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.*/
+
+
+
+/*
+var a == "dev"
+var b == "Lab"
+
+var name == a + b
+*/
+let a = "dev";
+let b = "Lab";
+
+let name = a + b;
